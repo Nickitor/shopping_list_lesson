806e5aa67e89e1a20f8714bde03cce0ebef3c100
Nikita Zamyslov <38568177+Nickitor@users.noreply.github.com>
2022-09-28 18:55:25 +0300

    Refactor

--- app/src/main/java/com/nikitazamyslov/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/nikitazamyslov/shoppinglist/presentation/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         mainViewModel.shopList.observe(this) {
             println(it.toString())
